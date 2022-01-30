8cfc33c590c8a30aca3e77e0c2faafe3095ffa6c
MeaninGood <jsmeaningood@gmail.com>
2022-01-30 14:15:29 +0900

    22.01.30

--- 0128_S5_2609.py
+++ 0128_S5_2609.py
@@ -19,12 +19,33 @@
 
 n, m = map(int, input().split())
 
+max_num = max(n, m)
+min_num = min(n, m)
+gcm_li = [] # 최대 공약수
+lcm_li = [] # 최소 공배수
+gcm = 0
+lcm = 999999999
+# def solve(n, m):
+   
+for i in range(1, min_num+1) :
+    if min_num % i == 0 :
+        gcm_li.append(i)
+            
+for gnum in gcm_li :
+    if (max_num % gnum == 0) and (gnum > gcm) :
+        gcm = gnum
+            
+            
+for j in range(1, max_num+1) :
+    k = j * max_num
+    lcm_li.append(k)
+        
+for lnum in lcm_li :
+    if (lnum % min_num == 0) and lcm > lnum :
+        lcm = lnum
+        break
 
-gcm = # 최대 공약수
-lcm = # 최소 공배수
-def solve(n, m):
-    if n > m :
-        for i in range(m) :
+print(gcm, lnum, sep = '\n')
+
             
-            
-solve(n, m)
+
