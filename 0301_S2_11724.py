a4368f8b9125bbc3461280ad29accef4143b48a6
MeaninGood <jsmeaningood@gmail.com>
2022-03-03 23:58:19 +0900

    22.03.03

--- 0301_S2_11724.py
+++ 0301_S2_11724.py
@@ -1,28 +1,54 @@
+# 11724번_연결 요소의 개수
+
+## 방향 없는 그래프가 주어졌을 때, 연결 요소의 개수 구하기
+
+'''
+# 첫째 줄에 정점의 개수 N과 간선의 개수 M
+# (1 ≤ N ≤ 1,000, 0 ≤ M ≤ Nx(N-1)/2) 
+# 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v
+# (1 ≤ u, v ≤ N, u ≠ v)
+# 같은 간선은 한 번만 주어짐
+## 첫째 줄에 연결 요소의 개수를 출력
+
+
+(입력)
+6 5
+1 2
+2 5
+5 1
+3 4
+4 6
+
+(출력) 
+2
+
+'''
+
 def dfs(cur):
     visited[cur] = True
     
-    for nxt in v[cur]:
-        if visited[nxt]:
+    for nxt in v[cur]: # 연결된 애들을 돌면서
+        if visited[nxt]: # True면 continue
             continue
         
-        dfs(nxt)
+        dfs(nxt) # 아니면 다 True로 바꿔줌
 
 
 n, m = map(int, input().split())
 
-v = [[] for i in range(n+1)]
+v = [[] for i in range(n+1)] # 인덱스 맞추려고 그냥 n+1해줌
 for i in range(m):
     a, b = map(int, input().split())
     
-    v[a].append(b)
+    v[a].append(b) # 무향이니까 둘 다 추가
     v[b].append(a)
     
 visited = [False for i in range(n+1)]
 
 cnt = 0
 for i in range(1, n+1):
-    if not visited[i]:
-        dfs(i)
-        cnt += 1
+    if not visited[i]: # False인 1번 인덱스부터 시작해서
+        dfs(i) # dfs(i)를 한 번 다 돌고 나면
+        cnt += 1 # cnt += 1씩 함, 다 True가 될 때까지
 
 print(cnt)
---
+++ 0303_S1_2468.py
@@ -0,0 +1,99 @@
+# 2468번_안전 영역
+
+## 어떤 지역의 높이 정보를 파악
+## 장마철에 내리는 비의 양에 따라 일정한 높이 이하의 모든 지점은 물에 잠긴다
+## 행과 열의 크기가 각각 N인 2차원 배열에서 배열의 각 원소는 해당 지점의 높이를 표시하는 자연수
+## 어떤 지역의 높이 정보가 주어졌을 때, 장마철에 물에 잠기지 않는 안전한 영역의 최대 개수를 계산
+
+'''
+# 첫째 줄에는 어떤 지역을 나타내는 2차원 배열의 행과 열의 개수를 나타내는 수 N이 입력
+# N은 2 이상 100 이하의 정수
+# 둘째 줄부터 N개의 각 줄에는 2차원 배열의 첫 번째 행부터 N번째 행까지 순서대로 한 행씩 높이 정보가 입력
+# 각 줄에는 각 행의 첫 번째 열부터 N번째 열까지 N개의 높이 정보를 나타내는 자연수가 빈 칸을 사이에 두고 입력
+# 높이는 1이상 100 이하의 정수
+## 첫째 줄에 장마철에 물에 잠기지 않는 안전한 영역의 최대 개수를 출력
+
+
+(입력)
+5
+6 8 2 6 2
+3 2 3 4 6
+6 7 3 3 2
+7 2 5 3 6
+8 9 5 2 7
+
+(출력) 
+5
+
+'''
+
+# n = int(input())
+# arr = [list(map(int, input().split())) for i in range(n)]
+
+# visited = [[False]*110 for i in range(110)]
+
+# dx = [0, 1, 0, -1]
+# dy = [1, 0, -1, 0]
+# mx = 0
+# def dfs(x, y):
+#     global mx
+#     ret = 1
+#     i = 1
+#     visited[x][y] = True
+
+#     for j in range(4):
+#         nx = x + dx[j]
+#         ny = y + dy[j]
+        
+#         if not (0 <= nx < n and 0 <= ny < n) or visited[nx][ny] or arr[nx][ny] < i:
+#             continue
+    
+#         if ret > mx:
+#             mx = ret
+            
+#         ret += dfs(nx, ny)
+            
+#     visited[nx][ny] = False
+#     i += 1
+#     return mx
+
+# print(dfs(0,0))
+
+            
+n = int(input())
+arr = [list(map(int, input().split())) for i in range(n)]
+
+num = 0
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] > num:
+            num = arr[i][j]
+        
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def dfs(x, y):
+    visited[x][y] = True
+
+    for j in range(4):
+        nx = x + dx[j]
+        ny = y + dy[j]
+        
+        if not (0 <= nx < n and 0 <= ny < n) or visited[nx][ny] or arr[nx][ny] <= i:
+            continue
+
+        dfs(nx, ny)
+
+ans = 0
+for i in range(num+1):
+    visited = [[False]*n for _ in range(n)]
+    cnt = 0
+    for j in range(n):
+        for k in range(n):
+            if arr[j][k] > i and not visited[j][k] :
+                cnt += 1
+                dfs(j, k)
+            
+    ans = max(ans, cnt)
+            
+print(ans)
