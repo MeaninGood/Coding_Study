9e27a7c478d682ee28060405c5dba875955e4d7d
MeaninGood <jsmeaningood@gmail.com>
2022-05-13 18:47:13 +0900

    22.05.13

--- 0510_S2_18352.py
+++ 0510_S2_18352.py
@@ -1,48 +1,48 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
 
-def bfs(s):
-    que = deque()
-    visited = [False for _ in range(n + 1)]
+# def bfs(s):
+#     que = deque()
+#     visited = [False for _ in range(n + 1)]
     
-    que.append(s)
-    visited[s] = True
+#     que.append(s)
+#     visited[s] = True
     
-    ret = 0 
-    ans = []
-    while que:
-        size = len(que)
+#     ret = 0 
+#     ans = []
+#     while que:
+#         size = len(que)
         
-        for _ in range(size):
-            cur = que.popleft()
+#         for _ in range(size):
+#             cur = que.popleft()
             
-            if ret == k:
-                ans.append(cur)
+#             if ret == k:
+#                 ans.append(cur)
                 
-            for nxt in v[cur]:
-                if visited[nxt]:
-                    continue
+#             for nxt in v[cur]:
+#                 if visited[nxt]:
+#                     continue
                 
-                que.append(nxt)
-                visited[nxt] = True
+#                 que.append(nxt)
+#                 visited[nxt] = True
                 
-        ret += 1
-    return ans
+#         ret += 1
+#     return ans
 
-n, m, k, x = map(int, input().split())
+# n, m, k, x = map(int, input().split())
 
-v = [[] for _ in range(n + 1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    v[a].append(b)
+# v = [[] for _ in range(n + 1)]
+# for i in range(m):
+#     a, b = map(int, input().split())
+#     v[a].append(b)
 
-res = bfs(x)
+# res = bfs(x)
 
-if len(res) == 0:
-    print(-1)
+# if len(res) == 0:
+#     print(-1)
 
-else:
-    res.sort()
-    for i in res:
-        print(i)
+# else:
+#     res.sort()
+#     for i in res:
+#         print(i)
